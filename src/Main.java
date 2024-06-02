a97f15d530f3aabf995f69c6354bbcccf14f926d
Moisuk-Dmitri <moisuk.d@gmail.com>
2024-06-02 14:16:38 +0300

    homework done

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,53 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Task 1");
 
+        int sum = 0;
+        int[] expenses = { 64500, 78000, 62000, 45100, 67000 };
+
+        for (int i = 0; i < expenses.length; i++) {
+            sum += expenses[i];
+        }
+
+        System.out.println("Сумма трат за месяц составила " + sum + " рублей");
+
+        System.out.println("Task 2");
+
+        int minExpenses = expenses[0];
+        int maxExpenses = expenses[0];
+
+        for (int i = 1; i < expenses.length; i++) {
+            if (minExpenses > expenses[i]) {
+                minExpenses = expenses[i];
+            }
+            if (maxExpenses < expenses[i]) {
+                maxExpenses = expenses[i];
+            }
+        }
+
+        System.out.println("Минимальная сумма трат за неделю составила " + minExpenses + " рублей. Максимальная сумма трат за неделю составила " + maxExpenses + " рублей");
+
+        System.out.println("Task 3");
+
+        int averageExpenses = 0;
+
+        for (int i = 0; i < expenses.length; i++) {
+            averageExpenses += expenses[i];
+        }
+        averageExpenses /= expenses.length;
+
+        System.out.println("Средняя сумма трат за месяц составила " + averageExpenses + " рублей");
+
+        System.out.println("Task 4");
+
+        char[] reverseFullName = { 'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+
+        for (int i = 0; i < reverseFullName.length / 2; i++) {
+            char temp = reverseFullName[i];
+            reverseFullName[i] = reverseFullName[reverseFullName.length - 1 -i];
+            reverseFullName[reverseFullName.length - 1 -i] = temp;
+        }
+        System.out.println(Arrays.toString(reverseFullName));
     }
 }
